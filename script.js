88fb5c3ecb2801c12e3e7ab3f57982c0d51c9acc
João Pedro de Oliveira <jao07oliveiragames@gmail.com>
2025-01-20 10:48:31 -0300

    continuando o projeto insta devClub

---
+++ script.js
@@ -0,0 +1,10 @@
+let $imagem = document.querySelector(".troca-imagem");
+function trocaImagem() {
+  if ($imagem.style.opacity == 0) {
+    $imagem.style.opacity = 1;
+  } else {
+    $imagem.style.opacity = 0;
+  }
+}
+
+setInterval(trocaImagem, 3000);
